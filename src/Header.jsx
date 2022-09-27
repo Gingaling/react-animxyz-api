3d5534b95d7bbf80cea7d384d3959c84053ebd78
Ging M. Louie <ging.m.louie@gmail.com>
2022-09-27 11:10:06 -0700

    Commit additional style tweaks

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,16 @@
+// import { XyzTransition } from '@animxyz/react';
+import './styles.css';
+import './Header.css';
+
 export default function Header() {
-	return (
-		<div
-			className="page-hero"
-			xyz="fade small stagger ease-out-back duration-60">
-			<div className="hero-logo xyz-nested" />
+	return <div className="page-hero" xyz="fade small stagger ease-out-back duration-30">
+			<h1>AnimXYZ, CSS Demonstration API</h1>
+			<div className="hero-logo" xyz="rotate-right-100%">
+			<img className="wormy" alt="Purple-Wormy" src="https://i.imgur.com/13U8rKd.png" />			
+			</div>
 			<p className="hero-text xyz-nested">
-				Curabitur blandit tempus porttitor. Morbi leo risus.
+				Purple wormy is curious to see how easy (or hard) it is to use
+				AnimXYZ.
 			</p>
-		</div>
-	);
+		</div>;
 }
